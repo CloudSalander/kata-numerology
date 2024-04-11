c1336d89bb3ccb23bf25351d4a26c661e647f478
Cloud <ruben.as532@gmail.com>
2024-04-11 10:59:35 +0200

    Apparently working with number reductions(442 bug?)

--- index.php
+++ index.php
@@ -2,26 +2,36 @@
 define("EXIT_CODE",-1);
 
 function sumFigures(int $number): int {
-
+    $sum = 0;
+    while(($number / 10) > 0) {
+        $sum += $number % 10;
+        $number /= 10;
+    }
+    return $sum + $number % 10;
 }
 
 function reduceNumber(int $number): int {
-
+    $sum_figures = $number;
+    while ($sum_figures >= 10) {
+        $sum_figures = sumFigures($number);
+    }
+    return $sum_figures;
 }
 
-function getNumberMeaning(int $number): string {
+function getNumberMeaning(int $number): void {
     
 }
 
 function printNumberMeaning(int $number): void {
-
+    $reduced_number = reduceNumber($number);
+    var_dump($reduced_number);
 }
 
 $num = 0;
 while($num != EXIT_CODE) {
     $num = readline("Tell me a number! :)");
     //TODO: Input validation, now we are assuming that its a number!
-    getNumberMeaning(intval($num));
+    printNumberMeaning(intval($num));
 }
 
 ?>
