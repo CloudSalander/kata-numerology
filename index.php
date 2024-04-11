690cdec04a37fdaaac1e19b37f5b8d8166161449
Cloud <ruben.as532@gmail.com>
2024-04-11 10:46:46 +0200

    Added function signatures

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
 <?php
 define("EXIT_CODE",-1);
 
+function sumFigures(int $number): int {
+
+}
+
+function reduceNumber(int $number): int {
+
+}
+
+function getNumberMeaning(int $number): string {
+    
+}
+
+function printNumberMeaning(int $number): void {
+
+}
+
 $num = 0;
 while($num != EXIT_CODE) {
     $num = readline("Tell me a number! :)");
+    //TODO: Input validation, now we are assuming that its a number!
+    getNumberMeaning(intval($num));
 }
 
 ?>
